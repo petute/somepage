5e73d545f4c11378a84a43fecb6ed1fd3a2785cd
Petute <daniel.petutschnigg@hotmail.com>
2020-07-15 14:43:30 +0200

    Add AboutBlock
    Added an AboutBlock

--- home/models.py
+++ home/models.py
@@ -1,6 +1,31 @@
-from django.db import models
-
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.admin.edit_handlers import StreamFieldPanel
+
+from grapple.helpers import register_streamfield_block
+
+from grapple.models import (
+    GraphQLStreamfield,
+    GraphQLImage,
+    GraphQLString,
+)
+
+#Sections
+
+#Block to display a short introduction of myself
+
+class _S_AboutBlock(blocks.StructBlock):
+    name = blocks.CharBlock()
+    description = blocks.RichTextBlock()
+    picture = ImageChooserBlock()
+
+    graphql_fields = [
+        GraphQLString('name'),
+        GraphQLString('description'),
+        GraphQLImage('picture'),
+    ]
 
 
 class HomePage(Page):
